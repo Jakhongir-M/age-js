6061cfb7472732bf978e8e4e7bbd69151fccbd3a
Jakhongir Mirzakhmatov <94097414+Jakhongir-M@users.noreply.github.com>
2022-05-19 08:47:17 +0500

    first commit

---
+++ index.js
@@ -0,0 +1,10 @@
+let age = +prompt("Yoshiz nechida?");
+if (10 >= age && age < 50) {
+  alert("SALOM KICHKINTOY");
+} else if (11 <= age && 50 >= age) {
+  alert("SALOM MEHMON");
+} else if (51 <= age && age <= 120) {
+  alert("SALOM DODA");
+} else {
+  alert("ALDAMA :D");
+}
